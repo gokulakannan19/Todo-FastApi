dd9cc22afda4458ccd84a8e04554312c21d249eb
gokulakannan19 <goks.cloud19@gmail.com>
2025-10-28 17:13:41 +0000

    Database created

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from routes import todos
+from database import Base, engine
+import models
 
 
 app = FastAPI()
 
 app.include_router(todos.router)
+
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
